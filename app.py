1fece8aa6662dc7f75d5b45c45799e6c368df298
ankitrkhobragade <khobragadeankit78@gmail.com>
2025-07-18 16:36:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import serpapi
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import googlesearch
+from serpapi import googlesearch
 
 def compare(med_name):
     params = {
